c0a62e784f55a37d2b6d2e7f1aceefb0aa56c17e
Rah1m-7 <s384247@students.cdu.edu.au>
2025-01-17 01:19:05 +0930

    Update Question-01.py
    Hope this will work.

--- Question-01.py
+++ Question-01.py
@@ -27,10 +27,60 @@ def encrypt(m,n):
     with open('encrypted_text.txt', 'w') as file2:
         file2.write(encrypted_text)
 
+        
+def decrypt(m,n):
+    """Decrypt the content of a encrypted file"""
+    
+    with open('encrypted_text.txt', 'r') as file:
+        text = file.read()
+        
+    decrypted_text = ''
+    for char in text:
+        if char.isdigit() or not char.isalnum():
+            decrypted_text = decrypted_text + char
+        elif 'a' <= char <= 'm':
+            shift = n*m
+            new_char = chr(((ord(char) - ord('a') - shift) % 13) + ord('a'))
+            decrypted_text = decrypted_text + new_char
+        elif 'n' <= char <= 'z':
+            shift = n+m
+            new_char = chr(((ord(char) - ord('n') + shift) % 13) + ord('n'))
+            decrypted_text = decrypted_text + new_char
+        elif 'A' <= char <= 'M':
+            shift = n
+            new_char = chr(((ord(char) - ord('A') + shift) % 13) + ord('A'))
+            decrypted_text = decrypted_text + new_char
+        elif 'N' <= char <= 'Z':
+            shift = m ** 2
+            new_char = chr(((ord(char) - ord('N') - shift) % 13) + ord('N'))
+            decrypted_text = decrypted_text + new_char
+
+       
+
+    return decrypted_text
+
+
+def check(decrypted_text):
+    """Check if the decrypted text matches the original text."""
+    with open('raw_text.txt', 'r') as file1:
+        original_text = file1.read()
+        
+    print("Original Text: ", original_text)
+    print("Decrypted Text: ", decrypted_text)
+    #Check correctness
+    if original_text == decrypted_text:
+        print('Decryption correct')
+    else:
+        print('Error in decryption')
+        
+
 
 def main():
     #User inputs for n and m 
     n = int(input("Enter the value of n "))
     m = int(input("Enter the value of m "))
-    encrypt(n, m)
+    encrypt(n, m)
+    decrypted_text = decrypt(n, m)
+    check(decrypted_text)
+
 main()
